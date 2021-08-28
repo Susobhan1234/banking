2cb99d64e37f4974d07cb46d3531bb8b4394bdbc
Susobhan <smasanta12@gmail.com>
2021-08-28 12:12:43 +0530

    Pattern

---
+++ src/main/java/examples/Q5.java
@@ -0,0 +1,15 @@
+/*
+ 5. C program to count number of digits in an integer.
+ */
+
+import java.util.Scanner;
+
+public class Q5
+{
+    public static void main (String [] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String number = sc.nextLine();
+        System.out.println (number.length());
+    }
+}
